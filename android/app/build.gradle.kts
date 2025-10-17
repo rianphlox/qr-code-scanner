433f686f153e3b0e907525feac28570109c23e49
rianphlox <skellybrownston@gmail.com>
2025-10-18 00:08:56 +0100

    Changed package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.qr_code"
+    namespace = "com.qubators.qscan"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.14033849"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.qr_code"
+        applicationId = "com.qubators.qscan"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/qubators/qscan/MainActivity.kt
+++ android/app/src/main/kotlin/com/qubators/qscan/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.qr_code
+package com.qubators.qscan
 
 import io.flutter.embedding.android.FlutterActivity
 
